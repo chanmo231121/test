0858e2a74807759af42f58392c55b52a19e74065
chanmo2311211 <abczza@naver.com>
2023-12-07 18:44:33 +0900

    계산기 수정본

--- app/src/main/java/com/example/testproject/test2.kt
+++ app/src/main/java/com/example/testproject/test2.kt
@@ -1,31 +1,63 @@
 package com.example.testproject
 
+import android.annotation.SuppressLint
+
+@SuppressLint("SuspiciousIndentation")
+
 fun main(){
     println("정수를 '한글자'씩 입력해주세요")
+    var  culc = Calculator()
+
 while(true) {
     try {
-
-
-        val num1 = readLine()!!.toInt()
+        val num1 = readLine()!!.toDouble()
         val op = readLine()
-        val num2 = readLine()!!.toInt()
+        val num2 = readLine()!!.toDouble()
+        var result = 0.0
 
-        val result =
             when (op) {
-                "+" -> num1 + num2
-                "-" -> num1 - num2
-                "*" -> num1 * num2
-                "/" -> num1 / num2
+                "+" -> result = culc.add(AddOperation(),num1,num2)
+                "-" -> result = culc.subtract(SubtractOperation(),num1,num2)
+                "*" -> result = culc.multi(MultiplyOperation(),num1,num2)
+                "/" -> result = culc.divide(DivideOperation(),num1,num2)
                 else -> {
                     println("연산자를 제대로 입력해주세요")
                 }
             }
-
-
-
         println("${num1} ${op} ${num2} = ${result}")
     } catch (e:java.lang.NumberFormatException) {
         println("정수를 입력하세요")
     }
 }
+}
+class Calculator{
+    fun add(sum:AddOperation , num1:Double , num2:Double):Double{
+        return sum.operate(num1,num2)
+    }
+    fun subtract(sub:SubtractOperation , num1:Double , num2:Double):Double{
+        return sub.operate(num1,num2)
+    }
+    fun multi(multi:MultiplyOperation , num1:Double , num2:Double):Double{
+        return multi.operate(num1,num2)
+    }
+    fun divide(div:DivideOperation , num1:Double , num2:Double):Double{
+        return div.operate(num1,num2)
+    }
+}
+
+
+class AddOperation{
+    fun operate(num1:Double,num2:Double):Double = (num1+num2).toDouble()
+}
+
+class SubtractOperation{
+    fun operate(num1:Double,num2:Double):Double = (num1-num2).toDouble()
+}
+
+class MultiplyOperation{
+    fun operate(num1:Double,num2:Double):Double = (num1*num2).toDouble()
+}
+
+class DivideOperation{
+    fun operate(num1:Double,num2:Double):Double = (num1/num2).toDouble()
 }
